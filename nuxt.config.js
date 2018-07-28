6cfd6c471f8d92a25083a1a0cf7a2900ffd03c0d
thaycacac <thaycacac@gmail.com>
2018-07-29 01:05:04 +0700

    done with storyblok

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,6 +18,10 @@ module.exports = {
   ** Customize the progress bar color
   */
   loading: { color: '#3B8070' },
+  modules: [
+    ['storyblok-nuxt',
+    { accessToken: 'yMt8VFkP652XioMfz8Gnngtt', cacheProvider: 'memory' }]
+  ],
   /*
   ** Build configuration
   */
--- pages/index.vue
+++ pages/index.vue
@@ -6,35 +6,56 @@
       :title="post.title"
       :excerpt="post.previewText"
       :thumbnailImage="post.thumbnailUrl"
-      :id="post.id"/>
+      :id="post.id" />
   </section>
 </template>
 
 <script>
-import PostPreview from '../components/Blog/PostPreview';
+import PostPreview from "@/components/Blog/PostPreview";
 export default {
   components: {
     PostPreview
   },
-  data() {
-    return {
-      posts: [
-        {
-          title: 'A New Beginning',
-          previewText: 'This will be awesome, don\'t miss it',
-          thumbnailUrl: '../coding.jpg',
-          id: 'a-new-beginning'
-        },
-        {
-          title: 'A Second Beginning',
-          previewText: 'This will be awesome, don\'t miss it',
-          thumbnailUrl: '../coding.jpg',
-          id: 'a-second-beginning'
-        }
-      ]
-    }
+  asyncData(context) {
+    return context.app.$storyapi
+      .get("cdn/stories", {
+        version: "draft",
+        starts_with: "blog/"
+      })
+      .then(res => {
+        return {
+          posts: res.data.stories.map(bp => {
+            return {
+              id: bp.slug,
+              title: bp.content.title,
+              previewText: bp.content.summary,
+              thumbnailUrl: bp.content.thumbnail
+            };
+          })
+        };
+      });
   }
-}
+  // data() {
+  //   return {
+  //     posts: [
+  //       {
+  //         title: "A New Beginning",
+  //         previewText: "This will be awesome, don't miss it!",
+  //         thumbnailUrl:
+  //           "http://www.healthyfood.co.uk/wp-content/uploads/2015/01/Cherry-tomato-bocc-olive-basil-pasta.jpg",
+  //         id: "a-new-beginning"
+  //       },
+  //       {
+  //         title: "A Second Beginning",
+  //         previewText: "This will be awesome, don't miss it!",
+  //         thumbnailUrl:
+  //           "http://www.healthyfood.co.uk/wp-content/uploads/2015/01/Cherry-tomato-bocc-olive-basil-pasta.jpg",
+  //         id: "a-second-beginning"
+  //       }
+  //     ]
+  //   };
+  // }
+};
 </script>
 
 <style scoped>
@@ -51,4 +72,3 @@ export default {
   }
 }
 </style>
-
